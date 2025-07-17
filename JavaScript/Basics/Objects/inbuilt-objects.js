a01a5d9ccf4b9e0cb8b920443c5a4d09c2e7c3ac
Pratham23003 <81950334+Pratham23003@users.noreply.github.com>
2025-07-17 23:33:20 +0530

    July 17 - Inbuilt Objects(Math and Date)

---
+++ JavaScript/Basics/Objects/inbuilt-objects.js
@@ -0,0 +1,32 @@
+//inbuilt objects
+
+//Math object
+//printing value of PI
+console.log(Math.PI);
+//getting maximum value
+console.log(Math.max(10,20,30,40,92,50,60,90));
+//getting minimum value 
+console.log(Math.max(10,20,30,40,92,50,60,90));
+//round off 
+console.log(Math.round(1.6));
+//greatest integer 
+console.log(Math.floor(1.6));
+//ceil returns greatest integer's round off (ig)
+console.log(Math.ceil(1.4));
+//absolute value -> +ve val stays +ve but -ve values are converted to +ve
+console.log(Math.abs(-2939));
+console.log(Math.abs(2939));
+//random number generator
+console.log(Math.random());
+//sqrt
+console.log(Math.sqrt(4));
+//power
+console.log(Math.pow(10,2));
+
+//Date Object
+let currentDate = new Date();
+console.log(currentDate);
+let birthDateTime = new Date('Decemeber 31 2003 2:30');
+console.log(birthDateTime);
+let newDate = new Date(2003, 11, 31, 2);
+console.log(newDate);
