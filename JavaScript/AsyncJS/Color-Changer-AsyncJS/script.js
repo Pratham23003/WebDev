cc4313285b2b8af73e8c09e9c1ab2045e39c2e90
Pratham23003 <81950334+Pratham23003@users.noreply.github.com>
2025-08-06 14:27:44 +0530

    6 Aug - AsyncJS Project Complete

--- JavaScript/AsyncJS/Color-Changer-AsyncJS/script.js
+++ JavaScript/AsyncJS/Color-Changer-AsyncJS/script.js
@@ -1,2 +1,28 @@
 //get DOM elements 
-const 
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const docBg = document.querySelector('body');
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+console.log(randomColor());
+
+let startId = 0;
+startBtn.addEventListener('click', ()=> {
+    startId = setInterval(() => {
+        docBg.style.backgroundColor = randomColor();
+    },200);
+}); 
+
+stopBtn.addEventListener('click', ()=> {
+    clearInterval(startId);
+    console.log("stopping now !");
+    startId = null; // good practice 
+});
