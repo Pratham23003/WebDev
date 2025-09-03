028e23cacd5a4ecebf0e2964f8fba9c20e105f9a
Pratham23003 <81950334+Pratham23003@users.noreply.github.com>
2025-09-03 21:56:37 +0530

    Sep 3 - Bookmark Saver Project

--- JavaScript/Projects/Bookmark-Saver/script.js
+++ JavaScript/Projects/Bookmark-Saver/script.js
@@ -1,33 +1,48 @@
 //dom elements
 
-//sitename Element 
-const siteNameEl = document.getElementById('site-name-input');
+//sitename Element
+const siteNameEl = document.getElementById("site-name-input");
+//form element
+const formEl = document.getElementById("bookmark-form");
 //site URL element
-const siteURLEl = document.getElementById('site-url-input');
-const submitBtn = document.getElementById('submit-btn');
+const siteURLEl = document.getElementById("site-url-input");
+const submitBtn = document.getElementById("submit-btn");
 //list container
-const listContainer = document.getElementById('bookmarks-list');
+const listContainer = document.getElementById("bookmarks-list");
 
+//clear the UI
+listContainer.innerHTML = "";
 //if theres something in the localStorage pull it up OR empty array init
-let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
-submitBtn.addEventListener('click', addBookmarks);
+submitBtn.addEventListener("click", addBookmarks);
 
-function addBookmarks(event){
-    event.preventDefault();
-    //get form values
-    const siteName = siteNameEl.value.trim();
-    const siteURL = siteURLEl.value.trim();
-    // console.log(siteName);
-    // console.log(siteURL);
+function addBookmarks(event) {
+  event.preventDefault();
+  //get form values
+  const siteName = siteNameEl.value.trim();
+  const siteURL = siteURLEl.value.trim();
+  // console.log(siteName);
+  // console.log(siteURL);
 
-    //pushing values to the bookmarks array
-    bookmarks.push({
-        id: Date.now,
-        siteName,
-        siteURL
-    });
-    //also updating the localStorage as well
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  //pushing values to the bookmarks array
+  bookmarks.push({
+    id: Date.now,
+    siteName,
+    siteURL,
+  });
+  //also updating the localStorage as well
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  //   console.log("About to execute update UI");
+  updateUI(siteName, siteURL);
 
+  //clear the values after function call
+  formEl.reset();
+}
+function updateUI(siteName, siteURL) {
+  const sortedBookmarks = [...bookmarks].reverse();
+  sortedBookmarks.forEach((bookmark) => {
+    const bookmarkLI = createBookmarkLI(bookmark);
+    
+  });
 }
