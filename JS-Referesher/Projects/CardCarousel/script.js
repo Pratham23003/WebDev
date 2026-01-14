1a4e1fdb874fd8f1ff4c94abc200cc3b13fb9456
Pratham23003 <pratham23003@gmail.com>
2026-01-14 21:02:17 +0530

    JS-Refresher/CardCarousel

---
+++ JS-Referesher/Projects/CardCarousel/script.js
@@ -0,0 +1,33 @@
+const users = [
+  {
+    name: "Aarohi Malhotra",
+    pic: "https://i.pinimg.com/736x/2a/3f/1b/2a3f1b6d2f0a1d2b3c4e5f6a7b8c9d0e.jpg",
+    bio: "soft heart, sharp mind ✨ | poetry & playlists"
+  },
+  {
+    name: "Rhea Kapoor",
+    pic: "https://i.pinimg.com/736x/9f/4b/6c/9f4b6c3d2a1e5f7b8c9d0a2b3e4f6d7.jpg",
+    bio: "living in pastel dreams 🌸 | late night thinker"
+  },
+  {
+    name: "Nisha Verma",
+    pic: "https://i.pinimg.com/736x/5e/1a/7d/5e1a7d3c9b0a2f4e6d8b1c7a5f9e3d.jpg",
+    bio: "chaos with a touch of class 💄 | coffee over everything"
+  },
+  {
+    name: "Kritika Sharma",
+    pic: "https://i.pinimg.com/736x/8c/2d/9e/8c2d9e1a5b6c3f7d4e0b9a8d2c1f6.jpg",
+    bio: "glam on the outside, depth on the inside ✨"
+  },
+  {
+    name: "Mehak Arora",
+    pic: "https://i.pinimg.com/736x/4b/7c/2a/4b7c2a9d6e1f8c5b0a3d7f2e9c4a.jpg",
+    bio: "quiet vibes, loud dreams 🌙 | not for everyone"
+  },
+  {
+    name: "Tanya Bansal",
+    pic: "https://i.pinimg.com/736x/1d/6f/3c/1d6f3c7a2b8e9d0c4f5a6b1e3a7.jpg",
+    bio: "too real for fake energy 🔥 | vibes only"
+  }
+];
+
