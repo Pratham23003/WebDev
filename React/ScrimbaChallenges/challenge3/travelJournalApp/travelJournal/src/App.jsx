20d3a78ab35288812e8a777b15df64969448bf24
Pratham23003 <pratham23003@gmail.com>
2026-03-03 00:30:39 +0530

    Scrimba-TravelJournal-Header&Card-Complete

---
+++ React/ScrimbaChallenges/challenge3/travelJournalApp/travelJournal/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header'
+import Card from './components/card'
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <Card></Card>
+    </>
+  )
+}
+
+export default App
---
+++ React/ScrimbaChallenges/challenge3/travelJournalApp/travelJournal/src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div>
+      <div className="header-container">
+        <div className="logo-container">
+          <img src="./globe.png" alt="Globe Icon" width="24px"/>
+          <h3 style={{fontWeight: 500}}>my travel journal.</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
