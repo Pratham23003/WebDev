a72abbcbf0b15f32adb9ddb264dde8ec75c493eb
Pratham23003 <pratham23003@gmail.com>
2026-03-22 22:30:57 +0530

    Tailwind-NetflixClone-TrendingSection:Updated

--- TailwindCSS/04/netflix-clone/src/components/MovieCard.jsx
+++ TailwindCSS/04/netflix-clone/src/components/MovieCard.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
   return (
-    <div className="min-w-[150px] md:min-w-[200px] aspect-[2/3] shrink-0 cursor-pointer overflow-hidden rounded-md hide-scrollbar">
-      <img src={movie.img} alt={movie.alt} className="w-full h-full object-cover rounded-md transition-transform hover:scale-110" />
-      {/* <p className="relative bg-amber-500 "></p> */}
+    <div>
+
+      <p className="relative text-black top-65 right-3 h-10 w-10 z-[1] text-8xl font-extrabold stroke-white">
+        {movie.id}
+      </p>
+
+      <div className="min-w-[150px] md:min-w-[200px] aspect-[2/3] shrink-0 cursor-pointer overflow-hidden rounded-md hide-scrollbar">
+        <img
+          src={movie.img}
+          alt={movie.alt}
+          className="w-full h-full object-cover rounded-md transition-transform hover:scale-110"
+        />
+      </div>
+
     </div>
   );
 };
