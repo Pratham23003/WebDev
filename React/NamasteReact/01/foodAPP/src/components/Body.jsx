63d7ba7aedee34730d4d92235ef8cb90b1ef7d2a
Pratham23003 <pratham23003@gmail.com>
2026-04-11 13:04:30 +0530

    NamasteReact-FoodAPP: Props Added

--- React/NamasteReact/01/foodAPP/src/components/Body.jsx
+++ React/NamasteReact/01/foodAPP/src/components/Body.jsx
@@ -2,14 +2,84 @@ import React from 'react'
 import RestaurantCard from './RestaurantCard'
 
 const Body = () => {
+
+  const resData = 
+  [{
+    id: 1,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: "./startbuck.webp"
+  },
+  {
+    id: 2,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: ""
+  },
+  {
+    id: 3,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: ""
+  },
+  {
+    id: 4,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: ""
+  },
+  {
+    id: 4,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: ""
+  },
+  {
+    id: 4,
+    resName: "Starbucks",
+    resRating : 5.0,
+    foodCategory : "Desserts • Fast Food",
+    priceDetails : "₹900 for two",
+    location: "Ferozpur Road, Ludhiana",
+    distance: 6.7,
+    bgURL: ""
+  }];
+
   return (
-    <div className='flex-col'>
-        <div id='search-container' className='items-center'>
+    <div className='mx-auto flex-col w-[80%]  items-center justify-center overflow-x-scroll'>
+        <div id='search-container' className='text-center mx-auto'>
           Search
         </div>
 
-        <div id='card-container'>
-          <RestaurantCard/>
+        <div id='card-container' className="flex gap-5">
+          {resData.map((res) => 
+          (
+            <RestaurantCard
+            key={res.id}
+            {...res} //we did this to prevent passing individual prop.
+            >
+            </RestaurantCard>
+          ))}
         </div>
     </div>
   )
--- React/NamasteReact/01/foodAPP/src/components/RestaurantCard.jsx
+++ React/NamasteReact/01/foodAPP/src/components/RestaurantCard.jsx
@@ -1,50 +1,48 @@
 import React from "react";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  // console.log(props);
   return (
     <div>
-
       <div className="h-100 w-90 border-none rounded-full cursor-pointer">
-
         <div className="">
-
-          <div className="img-container relative h-80 w-90 bg-[url('/startbuck.webp')] bg-cover bg-center">
+          <div
+            className="img-container relative h-80 w-90 bg-cover bg-center"
+            style={{ backgroundImage: `url(${props.bgURL})` }}
+          >
             {/* Gradient overlay */}
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
           </div>
 
           <div className="restaurant-title relative bottom-7">
-
             <div className="restaurant-heading-container flex justify-between">
-              <h3 className="font-bold text-white pl-3">Starbucks Coffee</h3>
+              <h3 className="font-bold text-white pl-3">{props.resName}</h3>
               <div className="rating-container">
-                <span className="text-white pr-3">5.0</span>
+                <span className="text-white pr-3">{props.resRating}</span>
               </div>
             </div>
-
           </div>
 
           <div className="upper-details-container flex justify-between">
-            <div className="location-details-container ">
-              <span>Desserts • Fast Food</span>
+            <div className="foodCategory-details-container ">
+              <span>{props.foodCategory}</span>
             </div>
 
             <div className="price-details-container">
-              <span>₹900 for two</span>
+              <span>{props.priceDetails}</span>
             </div>
           </div>
 
           <div className="lower-details-container flex justify-between">
             <div className="location-container ">
-              <span>Ferozpur Road, Ludhiana</span>
+              <span>{props.location}</span>
             </div>
 
             <div className="distance-container">
-              <span>6.7 km</span>
+              <span>{props.distance}</span>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
